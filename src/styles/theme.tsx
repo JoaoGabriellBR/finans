3d3eb29f5f9af7f2cde819a504a0b5011547e8db
João Gabriel <joaoname9@gmail.com>
2023-06-25 01:34:23 -0300

    add theme.tsx

---
+++ src/styles/theme.tsx
@@ -0,0 +1,10 @@
+import { extendTheme } from "@chakra-ui/react";
+
+const theme = extendTheme({
+  fonts: {
+    body: "Codec Regular, sans-serif",
+    heading: "Codec Regular, sans-serif",
+  },
+});
+
+export default theme;
