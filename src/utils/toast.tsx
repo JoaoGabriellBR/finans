42eb43149e9344b41080b09c60094ff4b0ff6f1a
João Gabriel <joaoname9@gmail.com>
2023-06-25 17:46:03 -0300

    add toast notification

---
+++ src/utils/toast.tsx
@@ -0,0 +1,11 @@
+const notification = (toast: any, message: any, status: any) => {
+  toast({
+    description: message,
+    status: status,
+    duration: 2000,
+    isClosable: true,
+    position: "top-right",
+  });
+};
+
+export default notification;
