d7434b0dd303e81e03aa3a3d8c00190184e1396c
João Gabriel <joaoname9@gmail.com>
2023-07-06 13:01:42 -0300

    refactor: vite api url

--- src/api/index.tsx
+++ src/api/index.tsx
@@ -1,7 +1,9 @@
-import axios from "axios";
+import axios from 'axios';
+
+const apiURL = import.meta.env.VITE_API_URL;
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL,
+  baseURL: apiURL,
 });
 
 export default api;
