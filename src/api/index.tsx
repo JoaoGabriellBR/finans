dab5a223104b623ca1876c3317adecf07f01f802
João Gabriel <joaoname9@gmail.com>
2023-07-06 04:18:06 -0300

    refactor: adding dotenv

--- src/api/index.tsx
+++ src/api/index.tsx
@@ -1,6 +1,6 @@
-import axios from "axios";
+import axios, { AxiosInstance } from "axios";
 
-const api = axios.create({
+const api: AxiosInstance = axios.create({
   baseURL: process.env.REACT_APP_API_URL,
 });
 
